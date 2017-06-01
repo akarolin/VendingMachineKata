8719cf36afad27103344aa3ab888b253572272ba
avk100 <andy.karolin@gmail.com>
2017-06-01 10:40:00 -0400

    Initial Commit

---
+++ VendingMachineKata/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VendingMachineKata
+//
+//  Created by Andy Karolin on 6/1/17.
+//  Copyright © 2017 AccendantC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
