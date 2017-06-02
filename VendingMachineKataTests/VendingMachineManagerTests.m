99765dd2d37673eca94af7b34b3b27a76551b32e
avk100 <andy.karolin@gmail.com>
2017-06-02 14:36:05 -0400

    Select Product Feature Request Refactored out the cut and paste craziness from VendingMachineManagerTest

--- VendingMachineKataTests/VendingMachineManagerTests.m
+++ VendingMachineKataTests/VendingMachineManagerTests.m
@@ -11,13 +11,15 @@
 
 @interface VendingMachineManagerTests : XCTestCase
 
+@property (strong, nonatomic) VendingMachineManager *vendingMachineManager;;
+
 @end
 
 @implementation VendingMachineManagerTests
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.vendingMachineManager = [[VendingMachineManager alloc] init];
 }
 
 - (void)tearDown {
@@ -26,107 +28,102 @@
 }
 
 - (void)testValidCoin {
-    VendingMachineManager *vendingMachineManager = [[VendingMachineManager alloc] init];
-    [vendingMachineManager insertCoin:Nickel];
-    XCTAssertTrue([vendingMachineManager pennyAmountOfCoinsInput] == 5);
+    [self.vendingMachineManager insertCoin:Nickel];
+    XCTAssertTrue([self.vendingMachineManager pennyAmountOfCoinsInput] == 5);
 
-    [vendingMachineManager insertCoin:Dime];
-    XCTAssertTrue([vendingMachineManager pennyAmountOfCoinsInput] == 15);
+    [self.vendingMachineManager insertCoin:Dime];
+    XCTAssertTrue([self.vendingMachineManager pennyAmountOfCoinsInput] == 15);
 
-    [vendingMachineManager insertCoin:Quarter];
-    XCTAssertTrue([vendingMachineManager pennyAmountOfCoinsInput] == 40);
+    [self.vendingMachineManager insertCoin:Quarter];
+    XCTAssertTrue([self.vendingMachineManager pennyAmountOfCoinsInput] == 40);
 }
 
 - (void)testInvalidCoin {
-    VendingMachineManager *vendingMachineManager = [[VendingMachineManager alloc] init];
-    [vendingMachineManager insertCoin:Penny];
-    XCTAssertTrue([vendingMachineManager pennyAmountOfCoinsReturned] == 1);
+    [self.vendingMachineManager insertCoin:Penny];
+    XCTAssertTrue([self.vendingMachineManager pennyAmountOfCoinsReturned] == 1);
     
-    [vendingMachineManager insertCoin:Penny];
-    XCTAssertTrue([vendingMachineManager pennyAmountOfCoinsReturned] == 2);
+    [self.vendingMachineManager insertCoin:Penny];
+    XCTAssertTrue([self.vendingMachineManager pennyAmountOfCoinsReturned] == 2);
 
 }
 
 - (void)testCoinsInput {
-    VendingMachineManager *vendingMachineManager = [[VendingMachineManager alloc] init];
     NSUInteger total = 0;
     
-    [vendingMachineManager insertCoin:Nickel];
-    total = [vendingMachineManager pennyAmountOfCoinsInput];
+    [self.vendingMachineManager insertCoin:Nickel];
+    total = [self.vendingMachineManager pennyAmountOfCoinsInput];
     XCTAssertTrue(total == 5);
     
-    [vendingMachineManager insertCoin:Dime];
-    total = [vendingMachineManager pennyAmountOfCoinsInput];
+    [self.vendingMachineManager insertCoin:Dime];
+    total = [self.vendingMachineManager pennyAmountOfCoinsInput];
     XCTAssertTrue(total == 15);
     
-    [vendingMachineManager insertCoin:Quarter];
-    total = [vendingMachineManager pennyAmountOfCoinsInput];
+    [self.vendingMachineManager insertCoin:Quarter];
+    total = [self.vendingMachineManager pennyAmountOfCoinsInput];
     XCTAssertTrue(total == 40);
     
-    [vendingMachineManager insertCoin:Penny];
-    total = [vendingMachineManager pennyAmountOfCoinsInput];
+    [self.vendingMachineManager insertCoin:Penny];
+    total = [self.vendingMachineManager pennyAmountOfCoinsInput];
     XCTAssertTrue(total == 40);
 }
 
 - (void)testCoinsReturned {
-    VendingMachineManager *vendingMachineManager = [[VendingMachineManager alloc] init];
     NSUInteger total = 0;
     
-    [vendingMachineManager insertCoin:Penny];
-    total = [vendingMachineManager pennyAmountOfCoinsReturned];
+    [self.vendingMachineManager insertCoin:Penny];
+    total = [self.vendingMachineManager pennyAmountOfCoinsReturned];
     XCTAssertTrue(total == 1);
     
-    [vendingMachineManager insertCoin:Penny];
-    total = [vendingMachineManager pennyAmountOfCoinsReturned];
+    [self.vendingMachineManager insertCoin:Penny];
+    total = [self.vendingMachineManager pennyAmountOfCoinsReturned];
     XCTAssertTrue(total == 2);
     
-    [vendingMachineManager insertCoin:Nickel];
-    total = [vendingMachineManager pennyAmountOfCoinsReturned];
+    [self.vendingMachineManager insertCoin:Nickel];
+    total = [self.vendingMachineManager pennyAmountOfCoinsReturned];
     XCTAssertTrue(total == 2);
     
-    [vendingMachineManager insertCoin:Dime];
-    total = [vendingMachineManager pennyAmountOfCoinsReturned];
+    [self.vendingMachineManager insertCoin:Dime];
+    total = [self.vendingMachineManager pennyAmountOfCoinsReturned];
     XCTAssertTrue(total == 2);
     
-    [vendingMachineManager insertCoin:Quarter];
-    total = [vendingMachineManager pennyAmountOfCoinsReturned];
+    [self.vendingMachineManager insertCoin:Quarter];
+    total = [self.vendingMachineManager pennyAmountOfCoinsReturned];
     XCTAssertTrue(total == 2);
     
-    [vendingMachineManager insertCoin:Penny];
-    total = [vendingMachineManager pennyAmountOfCoinsReturned];
+    [self.vendingMachineManager insertCoin:Penny];
+    total = [self.vendingMachineManager pennyAmountOfCoinsReturned];
     XCTAssertTrue(total == 3);
 }
 
 - (void)testTakeChange {
-    VendingMachineManager *vendingMachineManager = [[VendingMachineManager alloc] init];
     NSUInteger total;
     
-    [vendingMachineManager insertCoin:Penny];
-    [vendingMachineManager insertCoin:Penny];
-    [vendingMachineManager insertCoin:Penny];
+    [self.vendingMachineManager insertCoin:Penny];
+    [self.vendingMachineManager insertCoin:Penny];
+    [self.vendingMachineManager insertCoin:Penny];
     
-    total = [vendingMachineManager pennyAmountOfCoinsReturned];
+    total = [self.vendingMachineManager pennyAmountOfCoinsReturned];
     XCTAssertTrue(total == 3);
 
-    total = [vendingMachineManager takeChange];
+    total = [self.vendingMachineManager takeChange];
     XCTAssertTrue(total == 3);
 
-    total = [vendingMachineManager pennyAmountOfCoinsReturned];
+    total = [self.vendingMachineManager pennyAmountOfCoinsReturned];
     XCTAssertTrue(total == 0);
 
 }
 
 - (void)testCanBuyProduct {
-    VendingMachineManager *vendingMachineManager = [[VendingMachineManager alloc] init];
     Product *product = [[Product alloc] initProductName:@"Chips" withPrice:50];
 
-    XCTAssertFalse([vendingMachineManager canBuyProduct:product]);
-    [vendingMachineManager insertCoin:Quarter];
-    XCTAssertFalse([vendingMachineManager canBuyProduct:product]);
-    [vendingMachineManager insertCoin:Quarter];
-    XCTAssertTrue([vendingMachineManager canBuyProduct:product]);
-    [vendingMachineManager insertCoin:Quarter];
-    XCTAssertTrue([vendingMachineManager canBuyProduct:product]);
+    XCTAssertFalse([self.vendingMachineManager canBuyProduct:product]);
+    [self.vendingMachineManager insertCoin:Quarter];
+    XCTAssertFalse([self.vendingMachineManager canBuyProduct:product]);
+    [self.vendingMachineManager insertCoin:Quarter];
+    XCTAssertTrue([self.vendingMachineManager canBuyProduct:product]);
+    [self.vendingMachineManager insertCoin:Quarter];
+    XCTAssertTrue([self.vendingMachineManager canBuyProduct:product]);
 
 }
+
 @end
